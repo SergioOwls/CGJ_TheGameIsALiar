019435dde9682ee896581c13bcb05f7efc28c236
Svetlin Svilenov <s.e.svilenov@gmail.com>
2019-08-25 22:04:16 +0300

    Rudimentary movement system added

---
+++ CGJ_LIAR/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject followTarget;
+
+    private void Update()
+    {
+        transform.position = followTarget.transform.position;
+        transform.eulerAngles = followTarget.transform.eulerAngles;
+    }
+}
---
+++ CGJ_LIAR/Assets/Scripts/DroneMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 movementH = transform.forward * v * Time.deltaTime * movementSpeed;
+        Vector3 movementV = transform.right * h * Time.deltaTime * movementSpeed;
+
+        Vector3 movement = movementH + movementV;
+
+        Vector3 rotation = new Vector3(0f, mouseX, 0f) * rotationSpeed * Time.deltaTime;
+
+        transform.position += movement;
+        transform.eulerAngles += rotation;
+    }
+}
