00e0119fe0992257ab66f1a6be160ca5263a02c7
Sergio R Perez Cuevas <sergio.perez7@upr.edu>
2019-08-29 17:01:06 -0400

    adding gun SFX / setting up scene

--- CGJ_LIAR/Assets/Scripts/GunLauncher.cs
+++ CGJ_LIAR/Assets/Scripts/GunLauncher.cs
@@ -5,61 +5,72 @@ using UnityEngine;
 public class GunLauncher : MonoBehaviour
 {
     public Transform cam;
+    public AudioClip[] gunClips;
+
     [Range(0,.5f)]  public float shakeAmplitude;
 
-    private ParticleSystem[] vfx;
-    private bool isShooting;
+    private ParticleSystem[] gunVFX;
+    private AudioSource gunSFX;
+    private bool isShooting, isReloading;
 
     private void Start()
     {
-        vfx = GetComponentsInChildren<ParticleSystem>();
+        gunVFX = GetComponentsInChildren<ParticleSystem>();
+        gunSFX = GetComponent<AudioSource>();
+
         isShooting = false;
+        isReloading = false;
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") & !isReloading)
             StartGun();
 
         if (Input.GetButtonUp("Fire1"))
-            StopGun();
-
+            isShooting = false;
     }
 
-    // If a particle collides, throw "hit" particle effect
-    // ?
     private void StartGun()
     {
-        if (!vfx[0].isPlaying)                      
-            foreach (ParticleSystem ps in vfx)      // Play particles.
+        isShooting = true;
+        gunSFX.clip = gunClips[0];
+
+        if (!gunVFX[0].isPlaying)                      
+            foreach (ParticleSystem ps in gunVFX)
                 ps.Play();
 
-        isShooting = true;
-
-        StartCoroutine(ShakeCam());
+        StartCoroutine(Shooting());
     }
 
-    private void StopGun()
-    {
-        foreach (ParticleSystem ps in vfx)
-            ps.Stop();
-
-        isShooting = false;
-    }
-
-    private IEnumerator ShakeCam()
+    private IEnumerator Shooting()
     {
         Vector3 ogPos = cam.localPosition;
 
         while (isShooting)
         {
+            if (!gunSFX.isPlaying)
+                gunSFX.Play();
+
             float x = Random.Range(-1, 1) * shakeAmplitude;
             float y = Random.Range(-1, 1) * shakeAmplitude;
 
             cam.localPosition = new Vector3(x, y, ogPos.z);
-
             yield return null;
         }
+
+        isReloading = true;
+
         cam.localPosition = ogPos;
+
+        gunSFX.clip = gunClips[1];
+        gunSFX.Play();
+
+        foreach (ParticleSystem ps in gunVFX)
+            ps.Stop();
+
+        yield return new WaitForSeconds(1);
+
+        isReloading = false;
     }
 }
